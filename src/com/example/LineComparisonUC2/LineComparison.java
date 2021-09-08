20a26721b5e58d168504d7d66dfebdc28270b536
ghatikawaykole <ghatika99@gmail.com>
2021-09-08 12:37:09 +0530

    Line1 is compared with the line2

--- src/com/example/LineComparisonUC2/LineComparison.java
+++ src/com/example/LineComparisonUC2/LineComparison.java
@@ -1,18 +1,21 @@
 package com.example.LineComparisonUC2;
 public class LineComparison {
-        public static void main(String args[]) {
-            // lengh of the cordinate system
-            int X1 = 10, X2 = 20, Y1 = 12, Y2 = 27;
-            int A1 = 20, A2 = 22, B1 = 30, B2 = 35;
-            //   int lenghOfLine =0, lenghOfLine1 =0;
-            double lenghOfLine = (Math.sqrt((X2 - X1) ^ 2 + (Y2 - Y1) ^ 2));
-            double lenghOfLine1 = (Math.sqrt((A2 - A1) ^ 2 + (B2 - B1) ^ 2));
-            System.out.println("Length of the first line" + lenghOfLine);
-            System.out.println("Length of the second line" + lenghOfLine1);
+    public static void main(String args[]) {
+        // lengh of the cordinate system
+        int X1 = 10, X2 = 20, Y1 = 12, Y2 = 27;
+        int A1 = 20, A2 = 22, B1 = 30, B2 = 35;
+        //   int lenghOfLine =0, lenghOfLine1 =0;
+        int lenghOfLine = (int)(Math.sqrt((X2 - X1) ^ 2 + (Y2 - Y1) ^ 2));
+        int lenghOfLine1 = (int)(Math.sqrt((A2 - A1) ^ 2 + (B2 - B1) ^ 2));
+        System.out.println("Length of the first line" + lenghOfLine);
+        System.out.println("Length of the second line" + lenghOfLine1);
 //        System.out.println("Line are equal"+lenghOfLine.equals(lenghOfLine1);
-            if (lenghOfLine == lenghOfLine1)
-                System.out.println("Length is equal");
-            else
-                System.out.println("Length not equal");
-        } // Add UC2 code
+        if(lenghOfLine == lenghOfLine1)
+            System.out.println("Length is equal");
+       else if(lenghOfLine > lenghOfLine1)
+            System.out.println("Length of the line1 is greater than length of line2");
+        else
+            System.out.println("Length of line1 is smaller than length of line2");
+
+    }
 }

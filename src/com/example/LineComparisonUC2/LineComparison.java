dcf23833e77b4f169d2cc9feb3bb75eafa7fb8e3
ghatikawaykole <ghatika99@gmail.com>
2021-09-08 12:02:28 +0530

    Length of the line calculation

--- src/com/example/LineComparisonUC2/LineComparison.java
+++ src/com/example/LineComparisonUC2/LineComparison.java
@@ -1,7 +1,10 @@
 package com.example.LineComparisonUC2;
 public class LineComparison {
     public static void main(String args[]) {
-        System.out.println("Welcome to line comparison program");
-
+        // lengh of the cordinate system
+        int X1 = 10, X2 = 20, Y1 = 12, Y2 = 27;
+        //   int lenghOfLine =0, lenghOfLine1 =0;
+        double lenghOfLine = (Math.sqrt((X2 - X1) ^ 2 + (Y2 - Y1) ^ 2));
+        System.out.println("Length of the line" + lenghOfLine);
     }
 }

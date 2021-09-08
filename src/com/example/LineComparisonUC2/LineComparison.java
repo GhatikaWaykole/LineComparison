b71ec0061fc287030a3c8a42e3956d3048020db7
ghatikawaykole <ghatika99@gmail.com>
2021-09-08 11:58:21 +0530

    Line Comparison progrnm

---
+++ src/com/example/LineComparisonUC2/LineComparison.java
@@ -0,0 +1,7 @@
+package com.example.LineComparisonUC2;
+public class LineComparison {
+    public static void main(String args[]) {
+        System.out.println("Welcome to line comparison program");
+
+    }
+}

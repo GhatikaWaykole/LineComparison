a3bbf164659a54b25c841992d85eefe084ff860b
ghatikawaykole <ghatika99@gmail.com>
2021-09-15 11:08:38 +0530

    Adding the class and acess the method in public class

--- src/com/example/LineComparisonUC2/LineComparison.java
+++ src/com/example/LineComparisonUC2/LineComparison.java
@@ -1,21 +1,43 @@
 package com.example.LineComparisonUC2;
+
+class lineEquation{
+   private int X1,X2,Y1,Y2;
+   public int lenghthOfLine;
+    //method to compute the line length
+    public void getLineLength(int X1,int X2,int Y1,int Y2){
+        this.X1=X1;
+        this.X2=X2;
+        this.Y1=Y1;
+        this.Y2=Y2;
+        lenghthOfLine = (int)(Math.sqrt((X2 - X1) ^ 2 + (Y2 - Y1) ^ 2));
+    }
+    public int setLineLength(){
+        return  lenghthOfLine;
+    }
+}
+
 public class LineComparison {
     public static void main(String args[]) {
+        lineEquation line = new lineEquation();
         // lengh of the cordinate system
         int X1 = 10, X2 = 20, Y1 = 12, Y2 = 27;
         int A1 = 20, A2 = 22, B1 = 30, B2 = 35;
         //   int lenghOfLine =0, lenghOfLine1 =0;
-        int lenghOfLine = (int)(Math.sqrt((X2 - X1) ^ 2 + (Y2 - Y1) ^ 2));
+    /*    int lenghOfLine = (int)(Math.sqrt((X2 - X1) ^ 2 + (Y2 - Y1) ^ 2));
         int lenghOfLine1 = (int)(Math.sqrt((A2 - A1) ^ 2 + (B2 - B1) ^ 2));
         System.out.println("Length of the first line" + lenghOfLine);
         System.out.println("Length of the second line" + lenghOfLine1);
-//        System.out.println("Line are equal"+lenghOfLine.equals(lenghOfLine1);
+       System.out.println("Line are equal"+lenghOfLine.equals(lenghOfLine1);
         if(lenghOfLine == lenghOfLine1)
             System.out.println("Length is equal");
        else if(lenghOfLine > lenghOfLine1)
             System.out.println("Length of the line1 is greater than length of line2");
         else
-            System.out.println("Length of line1 is smaller than length of line2");
+            System.out.println("Length of line1 is smaller than length of line2");  */
+        // calling the class method to compute length of the line
+        line.getLineLength(10,40,60,80);
+        System.out.println("Length of the first line"+line.setLineLength());
+
 
     }
 }
